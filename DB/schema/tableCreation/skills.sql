faeced6286736f045c638e39342e0e0332b69909
Shawn <shawninder@gmail.com>
2011-09-10 17:54:14 -0400

    Corrected syntax in .sql files

--- DB/schema/tableCreation/skills.sql
+++ DB/schema/tableCreation/skills.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS Skills
 (
-	id INT NOT NULL AUTO_INCREMENT PRIMARY_KEY,
-	name NOT NULL COMMENT 'Language/framework/plateform/IDE name, simply',
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	name VARCHAR(50) NOT NULL COMMENT 'simply the name of the language (or software or other) concerned',
 	url VARCHAR(50)
 );
