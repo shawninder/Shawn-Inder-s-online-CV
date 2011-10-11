555e1abb14ff68d9d40da7772499ff025c04b09f
Shawn <shawninder@gmail.com>
2011-10-10 21:38:01 -0400

    completed languages table

--- DB/schema/tableCreation/languages.sql
+++ DB/schema/tableCreation/languages.sql
@@ -1,7 +1,9 @@
-CREATE TABLE IF NOT EXISTS HumanLanguages
+CREATE TABLE IF NOT EXISTS Languages
 (
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	name VARCHAR(50) NOT NULL,
-	comment VARCHAR(100),
-	selfEvaluation TEXT NOT NULL
+	shortName VARCHAR(25),
+	'comment' VARCHAR(100) COMMENT 'Something like "Mother tongue", "Scripted", "Object-Oriented, compiled"...',
+	selfEvaluation TEXT,
+	isHuman BOOL NOT NULL
 );
