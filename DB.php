dfd1b61c18167e006b0f04b63b365a15c9d788ae
Shawn <shawninder@gmail.com>
2011-11-22 23:06:23 -0500

    Added a small php file which connects to the DB and selects the correct database

---
+++ DB.php
@@ -0,0 +1,11 @@
+<?php
+	// Define DSN, username and password
+	$host = 'shawninder.99k.org';
+	$username_site = '39855_site';
+	$username_shawn = '39855_shawn';
+	$password_site  = '514rep';
+	$password_shawn  = '514rep';
+	 
+	$DB = mysql_connect($host, $username_site, $password_site) or die(mysql_error());
+	mysql_select_db("shawninder_99k_portfolio") or die(mysql_error());
+?>
