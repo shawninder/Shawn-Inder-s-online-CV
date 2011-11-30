ee5740c67ec9201b94ebbac50456c714c398594c
Shawn <shawninder@gmail.com>
2011-11-29 23:13:00 -0500

    Renaming a whole lot of stuff and starting styles from scratch with WebPutty

--- index.php
+++ index.php
@@ -8,24 +8,27 @@
 >
 	<head>
 		<meta http-equiv="Content-Type" content="text/html;charset=utf-8">
-		<title>Shawn Inder's Online Résumé</title>
+		<title>Shawn Inder's Interactive C.V.</title>
 		
-		<?php include("DB.php"); ?>
-		
-		<link type="text/css" href="css/smoothness/jquery-ui-1.8.16.custom.css" rel="stylesheet" />
-		<link type="text/css" href="css/header.css" rel="stylesheet" />
-		<link type="text/css" href="css/tabs.css" rel="stylesheet" />
-		<link type="text/css" href="css/interactive.css" rel="stylesheet" />
-		<link type="text/css" href="css/experiences.css" rel="stylesheet" />
-		<link type="text/css" href="css/skills.css" rel="stylesheet" />
-		<link type="text/css" href="css/lists.css" rel="stylesheet" />
+		<?php
+			include("DB.php");
+			function addEvaluation($nbStars, $description)
+			{
+				return("<img src=\"images/rated_" . $nbStars . ".png\" alt=\"" . $nbStars . " stars: " . $description . "\" title=\"" . $description . "\" />");
+			}
+		?>
+
+		<!-- Using WebPutty -->
+		<link rel="stylesheet" type="text/css" href="http://www.webputty.net/css/agtzfmNzc2ZpZGRsZXIMCxIEUGFnZRjVuysM" />
+		<script type="text/javascript">(function(w,d){if(w.location!=w.parent.location||w.location.search.indexOf('__preview_css__')>-1){var t=d.createElement('script');t.type='text/javascript';t.async=true;t.src='http://www.webputty.net/js/agtzfmNzc2ZpZGRsZXIMCxIEUGFnZRjVuysM';(d.body||d.documentElement).appendChild(t);}})(window,document);</script>
+		<!-- WebPutty end -->
 		
 		<script type="text/javascript" src="js/jquery-1.6.2.min.js"></script>
 		<script type="text/javascript" src="js/jquery-ui-1.8.16.custom.min.js"></script>
 		<script type="text/javascript" src="js/jquery.easing.3.1.js"></script>
 		<script type="text/javascript" src="js/jquery.cycle.all.latest.js"></script>
 		<script type="text/javascript">
-			function log() {
+			/*function log() {
 				window.console && console.log && console.log('[log] ' + Array.prototype.join.call(arguments,' '));
 			}
 			function adjustPrevNextButtons(isNext, zeroBasedSlideIndex, slideElement)
@@ -93,13 +96,13 @@
 						toMini(element);
 					case 'mini':
 					default:
-						if(referredBy.match(/language/))
+						if(referredBy.match(/skill/))
 						{
-							$('#languageDescription_' + referredBy, elementObject).toggle("blind", {"easing":"easeInOutCirc"}, "normal");
+							$('#skillDescription_' + referredBy, elementObject).toggle("blind", {"easing":"easeInOutCirc"}, "normal");
 						}
 						else
 						{
-							$('#experienceDescription_' + referredBy, elementObject).toggle("blind", {"easing":"easeInOutCirc"}, "normal");
+							$('#jobDescription_' + referredBy, elementObject).toggle("blind", {"easing":"easeInOutCirc"}, "normal");
 						}
 						break;
 				}
@@ -123,6 +126,7 @@
 				$('.ui-widget-content', element).toggle("blind", {"easing":"easeInOutCirc"}, "normal");
 				$('.expander-icon', element).toggleClass("ui-icon-plusthick")
 																		.toggleClass("ui-icon-minusthick");
+				log(element.attr('style'));
 
 				// Make linked elements 'referred'
 				var links = element.data('linkedTo');
@@ -146,35 +150,35 @@
 					var dataLength = data.length;
 					for(var i = 0; i < dataLength; ++i)
 					{
-						var language = data[i].language;
-						var languageID = "language_" + language;
-						var experience = data[i].experience;
-						var experienceID = "experience_" + experience;
-						var languageDescription = "<div id=\"languageDescription_" + languageID + "\" class=\"linkDescription\">" + data[i].description + "</div>";
-						var experienceDescription = "<div id=\"experienceDescription_" + experienceID + "\" class=\"linkDescription\">" + data[i].description + "</div>";
-						var languageElement = $('#' + languageID);
-						var experienceElement = $('#' + experienceID);
-						$('.ui-widget-content', languageElement).after(experienceDescription);
-						$('.ui-widget-content', experienceElement).after(languageDescription);
+						var skill = data[i].skill;
+						var skillID = "skill_" + skill;
+						var job = data[i].job;
+						var jobID = "job_" + job;
+						var skillDescription = "<div id=\"skillDescription_" + skillID + "\" class=\"linkDescription\">" + data[i].description + "</div>";
+						var jobDescription = "<div id=\"jobDescription_" + jobID + "\" class=\"linkDescription\">" + data[i].description + "</div>";
+						var skillElement = $('#' + skillID);
+						var jobElement = $('#' + jobID);
+						$('.ui-widget-content', skillElement).after(jobDescription);
+						$('.ui-widget-content', jobElement).after(skillDescription);
 						
-						if(languageElement.data('linkedTo'))
+						if(skillElement.data('linkedTo'))
 						{
-							languageElement.data('linkedTo').push(experienceID);
+							skillElement.data('linkedTo').push(jobID);
 						}
 						else
 						{
-							var newData = new Array(experienceID);
-							languageElement.data('linkedTo', newData);
+							var newData = new Array(jobID);
+							skillElement.data('linkedTo', newData);
 						}
 						
-						if(experienceElement.data('linkedTo'))
+						if(jobElement.data('linkedTo'))
 						{
-							experienceElement.data('linkedTo').push(languageID);
+							jobElement.data('linkedTo').push(skillID);
 						}
 						else
 						{
-							var newData = new Array(languageID);
-							experienceElement.data('linkedTo', newData);
+							var newData = new Array(skillID);
+							jobElement.data('linkedTo', newData);
 						}
 					}
 					$('.linkDescription').toggle();
@@ -183,7 +187,6 @@
 			
 			$(document).ready(function()
 			{
-				$('body').tabs();
 				$('.slideshow').each(function() {
 					var parent = $(this).parent();
 					$(this).cycle({
@@ -221,146 +224,171 @@
 					});
 				});
 				getLinkInfo();
-			});
+			});*/
 		</script>
-		<link rel="stylesheet" type="text/css" href="http://www.webputty.net/css/agtzfmNzc2ZpZGRsZXIMCxIEUGFnZRjVuysM" />
-		<script type="text/javascript">(function(w,d){if(w.location!=w.parent.location||w.location.search.indexOf('__preview_css__')>-1){var t=d.createElement('script');t.type='text/javascript';t.async=true;t.src='http://www.webputty.net/js/agtzfmNzc2ZpZGRsZXIMCxIEUGFnZRjVuysM';(d.body||d.documentElement).appendChild(t);}})(window,document);</script>
 	</head>
 	<body>
 		<div id="header">
-			<h1>Shawn Inder's Online Résumé</h1>
-			<a href="mailto:shawninder@gmail.com" title="Write me an e-mail">shawninder@gmail.com</a>
+			<h1>iCV: <a class="emailLink" href="mailto:shawninder@gmail.com" title="Write me an e-mail">Shawn Inder</a></h1>
 		</div>
-		<ul>
-			<li><a href="#interactive">Interactive</a></li>
-			<li><a href="#download">Download</a></li>
-			<li><a href="#print">Print</a></li>
+		
+		<ul id="moreOptions">
+			<li><a>Version Française</a></li>
+			<li><a>Print current view</a></li>
+			<li><a>Download CV</a></li>
 		</ul>
+		
+		<!--<ul class="slideshow">
+			<li><img class="PortraitPicture" src="images/shawn.jpg" alt="A picture of Shawn Inder" /></li>
+		</ul>-->
 
-		<div id="interactive">
-			<div id="container">
-				<div id="experiences" class="half">
-					<h2><a class="resetter">Experience</a></h2>
-					<ul id="ui-widget skills-list">
+		<div id="interactiveCV">
+			<div id="CVcontainer">
+				<div id="jobList" class="half">
+					<h2 class="breadcrumbs">Jobs</h2>
+					<ul>
 						<?php
-							$sql_getExperiences = "
+							$sql_getJobs = "
 								SELECT
-									E.id AS eid,
-									position,
-									startDate,
-									endDate,
-									logoPath,
-									name,
-									url,
-									description, 
-									location,
-									jobDescription
+									J.id AS jID,
+									J.title AS jTitle,
+									J.startDate AS jStartDate,
+									J.endDate AS jEndDate,
+									O.name AS oName,
+									O.location AS oLocation,
+									J.Description AS jDescription
 								FROM
-									Experiences AS E
+									Jobs AS J
 									INNER JOIN Organizations AS O
-										ON O.id = E.organization
+										ON O.id = J.organization
 								ORDER BY
-									startDate DESC;";
-							$experiences = mysql_query($sql_getExperiences);
-							while($experience = mysql_fetch_array($experiences))
+									jStartDate DESC;";
+							$jobs = mysql_query($sql_getJobs);
+							while($job = mysql_fetch_array($jobs))
 							{
-								$endDate = ($experience['endDate'])?$experience['endDate']:"now";
+								$endDate = ($job['jEndDate'])?$job['jEndDate']:"now";
+								
+								$sql_getImages = "
+									SELECT
+										src,
+										description
+									FROM
+										job_images
+									WHERE job = " . $job['jID'];
+								$images = mysql_query($sql_getImages);
+								$nbImages = $images?mysql_num_rows($images):0;
+
 								$sql_getReferrals = "
 									SELECT
-										RE.title AS title,
 										RE.body AS excerpt,
-										firstName,
-										lastName
+										P.firstName as authorFirstName,
+										P.lastName as authorLastName
 									FROM
 										Referrals AS R
-										INNER JOIN experience_referral_matrix AS ERM
-											ON ERM.referral = R.id
+										INNER JOIN job_referral_matrix AS JRM
+											ON JRM.referral = R.id
 										INNER JOIN Referral_excerpts AS RE
 											ON RE.referral = R.id
 										INNER JOIN Persons AS P
 											ON P.id = R.author
-									WHERE ERM.experience = " . $experience['eid'];
+									WHERE JRM.job = " . $job['jID'];
 								$referrals = mysql_query($sql_getReferrals);
-								echo("<li id=\"experience_" . $experience['eid'] . "\" class=\"stateful\">
-												<h3 class=\"ui-widget-header ui-corner-top ui-state-default expander\"><span class=\"ui-icon ui-icon-plusthick expander-icon\"></span>" . $experience['position'] . " <span class=\"dates\">(" . $experience['startDate'] . " » " . $endDate . ")</span><div style=\"clear: both;\"></div></h3>
-												<div class=\"ui-widget-content\">
-													<img class=\"profile_pic\" src=\"" . $experience['logoPath'] . "\" alt=\"" . $experience['name'] . "\" />
-													<p>" . $experience['jobDescription'] . "</p>");
-								$nbReferrals = mysql_num_rows($referrals);
-								if($nbReferrals > 1)
+								$nbReferrals = $referrals?mysql_num_rows($referrals):0;
+								
+								$sql_getLinks = "
+									SELECT
+										url,
+										title,
+										text
+									FROM
+										job_link_matrix
+									WHERE job = " . $job['jID'];
+								$links = mysql_query($sql_getLinks);
+								$nbLinks = $links?mysql_num_rows($links):0;
+								
+								echo("<li id=\"job_" . $job['jID'] . "\" class=\"job\">
+												<h3 class=\"header\">" . $job['jTitle'] . " <span class=\"dates\">(" . $job['jStartDate'] . " » " . $endDate . ")</span><div style=\"clear: both;\"></div></h3>
+												<div class=\"allEyesOnly\">");
+
+								// Images
+								if($nbImages > 0)
 								{
-									echo("	<h4>Feedback
-														(<span class=\"current\">1</span>/<span class=\"total\">" . $nbReferrals . "</span>)
-														<a class=\"prev ui-helper-hidden-accessible ui-state-default\" title=\"Previous referral\">
-															<span class=\"ui-icon ui-icon-circle-triangle-w\">Prev</span>
-														</a>
-														<a class=\"next ui-helper-hidden-accessible ui-state-default\" title=\"Next referral\">
-															<span class=\"ui-icon ui-icon-circle-triangle-e\">Next</span>
-														</a>
-													</h4>
-													<ul class=\"referrals slideshow\">");
+									echo("	<ul class=\"slideshow\">");
+									while($image = mysql_fetch_array($images))
+									{
+										echo("	<li><img class=\"profilePicture\" src=\"" . $image['src'] . "\" alt=\"" . $image['description'] . "\" /></li>");
+									}
+									echo("	</ul>");
+								}
+													
+								// Job description
+								echo("		<p class=\"jobDescription\">" . $job['jDescription'] . "</p>");
+								
+								// Referrals
+								if($nbReferrals > 0)
+								{
+									echo("	<ul class=\"slideshow\">");
 									while($referral = mysql_fetch_array($referrals))
 									{
 										echo("	<li>
-															<h5>" . $referral['title'] . "</h5>
 															<q>" . $referral['excerpt'] . "</q>
-															 - " . $referral['firstName'] . " " . $referral['lastName'] . "
+															 - " . $referral['authorFirstName'] . " " . $referral['authorLastName'] . "
 														</li>");
 									}
 									echo("	</ul>");
 								}
+								
+								// Links
+								if($nbLinks > 0)
+								{
+									echo("	<ul class=\"linkList\">");
+									while($link = mysql_fetch_array($links))
+									{
+										echo("	<li><a href=\"" . $link['url'] . "\" title=\"" . $link['title'] . "\">" . $link['text'] . "</a></li>");
+									}
+									echo("	</ul>");
+								}
+								
+								// Footer
 								echo("	</div>
-												<p class=\"ui-widget-footer ui-corner-bottom\">
-													<a href=\"" . $experience['url'] . "\" title=\"" . $experience['description'] . "\">" . $experience['name'] . "</a>, " . $experience['location'] . "
-												</p>
+												<p class=\"footer\">" . $job['oName'] . ", " . $job['oLocation'] . "</p>
 											</li>");
 							}
 						?>
 					</ul>
 				</div>
-				<div id="skills" class="half">
-					<h2><a class="resetter">Technical Skills</a></h2>
-					<ul class="ui-widget skills-list">
+				<div id="skillList" class="half">
+					<h2 class="breadcrumbs">Skills</h2>
+					<ul>
 						<?php
-							$sql_getLanguages = "
+							$sql_getSkills = "
 								SELECT
 									id,
 									shortName,
 									name,
-									comment,
+									history,
+									stars,
 									selfEvaluation
 								FROM
-									Languages
+									Skills
 								ORDER BY
-									isHuman DESC,
-									selfEvaluation DESC;";
-							$languages = mysql_query($sql_getLanguages);
-							while($language = mysql_fetch_array($languages))
+									stars DESC;";
+							$skills = mysql_query($sql_getSkills);
+							while($skill = mysql_fetch_array($skills))
 							{
-								echo("<li id=\"language_" . $language['id'] . "\" class=\"stateful\">");
-								echo("	<h3 class=\"ui-widget-header ui-corner-all ui-state-default expander\" title=\"" . $language['name'] . "\">
-													<span class=\"ui-icon ui-icon-plusthick expander-icon\">Expand section</span>");
-								echo(($language['shortName'] != "")?$language['shortName']:$language['name']);
-								echo("		<span class=\"selfEvaluation\">(" . $language['selfEvaluation'] . ")</span>
+								echo("<li id=\"skill_" . $skill['id'] . "\" class=\"skill\">
+												<h3 class=\"header\" title=\"" . $skill['name'] . "\">");
+								echo(($skill['shortName'] != "")?$skill['shortName']:$skill['name']);
+								echo("		<span class=\"stars\">" . addEvaluation($skill['stars'], $skill['selfEvaluation']) . "</span>
 													<div style=\"clear: both;\"></div>
 												</h3>
-												<div class=\"ui-widget-content\">" . $language['comment'] . "</div>
+												<div class=\"allEyesOnly\">" . $skill['history'] . "</div>
 											</li>");
 							}
 						?>
 					</ul>
 				</div>
 			</div>
-		</div>
-		<div id="download">
-			<ul>
-				<li><a href="cv.pdf" title="Download my résumé as PDF">PDF</a></li>
-				<li><a href="cv.doc" title="Download my résumé as a Word document">Word</a></li>
-				<li><a href="cv.rtf" title="Download my résumé as RTF">RTF</a></li>
-			</ul>
-		</div>
-		<div id="print">
-			<p>Coming soon</p>
 		</div>
 		<div style="clear: both;"> </div>
 	</body>
