8ddaaf0088e2d64013bb5ea44211f0a0e76ce204
Shawn <shawninder@gmail.com>
2011-12-24 04:32:51 -0500

    Finished first version of PDF

--- CV.pdf.php
+++ CV.pdf.php
@@ -21,6 +21,12 @@ class CV extends FPDF
 	function setHeaderStyles()
 	{
 		$this->SetTextColor(0, 0, 0);
+		$this->SetFont('Helvetica', 'BU', 12);
+	}
+	
+	function setNameStyles()
+	{
+		$this->SetTextColor(0, 0, 0);
 		$this->SetFont('Helvetica', 'B', 12);
 	}
 	
@@ -34,6 +40,12 @@ class CV extends FPDF
 	{
 		$this->SetTextColor(0, 0, 0);
 		$this->SetFont('Times', '', 10);
+	}
+	
+	function setQuoteStyles()
+	{
+		$this->SetTextColor(0, 0, 0);
+		$this->SetFont('Times', 'I', 10);
 	}
 	
 	function writeLanguage($name)
@@ -56,7 +68,7 @@ class CV extends FPDF
 		$author,
 		$authorMail)
 	{
-		$this->setHeaderStyles();
+		$this->setNameStyles();
 		$this->Write($this->hLh, $title . "    " . $dateStr);
 		$this->Ln();
 		$this->setDefaultStyles();
@@ -69,6 +81,24 @@ class CV extends FPDF
 		$this->Ln();
 	}
 	
+	function writeSkill($name, $history)
+	{
+		$this->setNameStyles();
+		$this->Write($this->nLh, $name);
+		$this->setDefaultStyles();
+		$this->Write($this->nLh, " : " . $history);
+		$this->Ln(10);
+	}
+	
+	function writeFeedback($name, $quote)
+	{
+		$this->setNameStyles();
+		$this->Write($this->nLh, $name);
+		$this->setQuoteStyles();
+		$this->Write($this->nLh, " : " . $quote);
+		$this->Ln(10);
+	}
+	
 	public $hLh = 12;
 	public $nLh = 5;
 }
@@ -78,16 +108,19 @@ $cv->AddPage();
 $cv->setTitleStyles();
 $cv->Write($cv->hLh, 'CV: ');
 $cv->setLinkStyles();
-$cv->Write($cv->hLh, 'Shawn Inder', 'mailto:shawninder@gmail.com');
+$cv->Write($cv->hLh, 'Shawn Inder');
 $cv->Ln();
 
 $cv->setNoteStyles();
+$cv->Cell(15);
 $cv->Write($cv->nLh, '#: 514-903-9082'); 
-$cv->Write($cv->nLh, 'For more details, visit ');
+$cv->Ln();
 $cv->setLinkStyles();
-$cv->Write($cv->nLh, 'my interactive CV', 'http://shawninder.99k.org');
-$cv->setNoteStyles();
-$cv->Write($cv->nLh, ' online!');
+$cv->Cell(15);
+$cv->Write($cv->nLh, 'shawninder@gmail.com', 'mailto:shawninder@gmail.com');
+$cv->Ln();
+$cv->Cell(15);
+$cv->Write($cv->nLh, 'http://shawninder.99k.org', 'http://shawninder.99k.org');
 $cv->Ln(10);
 
 $cv->setHeaderStyles();
@@ -126,7 +159,8 @@ $sql_getExperiences = "
 		INNER JOIN Organizations AS O
 			ON O.id = E.organization
 	ORDER BY
-		eStartDate DESC;";
+		eStartDate DESC
+	LIMIT 6;";
 $experiences = mysql_query($sql_getExperiences);
 while($experience = mysql_fetch_array($experiences))
 {
@@ -219,9 +253,38 @@ while($experience = mysql_fetch_array($experiences))
 	}
 }
 
+$cv->AddPage();
 $cv->setHeaderStyles();
 $cv->Write($cv->hLh, 'Skills');
-$cv->setDefaultStyles();
+$cv->Ln();
+$sql_getSkills = "
+	SELECT
+		id,
+		shortName,
+		name,
+		history,
+		stars,
+		selfEvaluation
+	FROM
+		Skills
+	ORDER BY
+		stars DESC
+	LIMIT 13;";
+$skills = mysql_query($sql_getSkills);
+while($skill = mysql_fetch_array($skills))
+{
+	$skillName = ($skill['shortName'] != "")?$skill['shortName']:$skill['name'];
+	$cv->writeSkill($skillName, $skill['history']);
+}
+
+$cv->setHeaderStyles();
+$cv->Write($cv->hLh, 'Personal skills');
+$cv->Ln();
+
+$cv->writeFeedback("Get's Things Done", "It was exceptional how quickly Shawn became a contributing member of our team. Within a matter of two weeks, he had already largely completed the development of our first product.");
+$cv->writeFeedback("Team Spirit", "Complementing Shawn's technical and intuitive abilities, he easily socially integrated within our group, including joining colleagues for lunch time card games and hacky.");
+$cv->writeFeedback("Forward Thinking", "He was able to listen carefully to our needs [and] even though he did not know our business very well, he found a few glitches in my reasoning and was able to make corrections before the work was started, lowering the amount of hours needed for this project.");
+$cv->writeFeedback("Positive Thinking", "He had an attitude which left no space for failure.");
 
 $cv->Output();
 
