312d0fc31c22675023605beefd6e8af975001434
Shawn <shawninder@gmail.com>
2012-03-28 23:02:02 -0400

    Move passwords to non-open-source config.php

--- DB.php
+++ DB.php
@@ -1,21 +1,18 @@
 <?php
-	// Define DSN, username and password
-	$host = 'localhost';
-	$username_site = '39855_site';
-	$username_shawn = '39855_shawn';
-	$password_site  = '514rep';
-	$password_shawn  = '514rep';
-	$databaseName = 'shawninder_99k_portfolio';
+
+	include("config.php");	// : $host = 'localhost'; $username = 'Pseudonym!'; $password  = '************************';
+	
+	$dbName = $databaseName_en;
 	$failureMessage = "<p>Something is going on with my web host which makes my website temporarily unavailable.<br />Sorry for the bother, please try again later.</p>";
 
 	
 	if($_SESSION['lang'] == 'fr')
 	{
-		$databaseName = 'shawninder_99k_portfoliofr';
+		$dbName = $databaseName_fr;
 		$failureMessage = "<p>Un problème du côté de mon hébergeur web rend mon site temporairement inaccessible.<br />Excusez l'inconvénient, s'il vous plaît réessayez plus tard.</p>";
 	}
 	
-	$DB = mysql_connect($host, $username_site, $password_site) or die($failureMessage);
-	mysql_select_db($databaseName) or die($failureMessage);
+	$DB = mysql_connect($host, $username, $password) or die($failureMessage);
+	mysql_select_db($dbName) or die($failureMessage);
 	mysql_set_charset('utf8');
 ?>
